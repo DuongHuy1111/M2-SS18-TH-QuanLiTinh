648161e2c93477ee3cfeab39fec0880b382eac70
DuongHuy1111 <huy.alex.1111@gmail.com>
2019-05-07 14:06:23 +0700

    Quan Li Tinh

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\City;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    public function index()
+    {
+        $cities = City::all();
+        return view('cities.list', compact('cities'));
+    }
+
+    public function create()
+    {
+        return view('cities.create');
+    }
+
+    public function store(Request $request)
+    {
+        $city = new City();
+        $city->name = $request->input('name');
+        $city->save();
+        return redirect()->route('cities.index');
+    }
+
+    public function edit(Request $request, $id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $city = City::findOrFail($id);
+            return view('cities.edit', compact('city'));
+
+        } else {
+            $city = City::findOrFail($id);
+            $city->name = $request->name;
+            $city->save();
+            return redirect()->route('cities.index');
+        }
+
+    }
+
+    public function destroy($id)
+    {
+        $city = City::findOrFail($id);
+        $city->delete();
+        return redirect()->route('cities.index');
+    }
+}
